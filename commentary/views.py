64da45080e504e5155e685a16546f942fe8c6db6
Kiidle <jonas.buechi@4teamwork.ch>
2023-03-08 13:44:59 +0100

    Delete Commentary

--- commentary/views.py
+++ commentary/views.py
@@ -1,7 +1,25 @@
+from django.shortcuts import redirect
 from django.views import generic
 
+import commentary
 from commentary.models import Commentary
 
 class CommentaryView(generic.DetailView):
     model = Commentary
     template_name = "commentary/commentary.html"
+
+def commentary_create(request):
+    return
+
+def commentary_edit(request, pk):
+    return
+
+def commentary_delete(request, pk):
+    commentary = Commentary.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        feed_id = commentary.feed.id
+        commentary.delete()
+        return redirect('feed', pk=feed_id)
+
+    return (request, 'commentary/delete.html', {'commentary': commentary})

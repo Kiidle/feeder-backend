2aabe4d03a2a6212cb43940e6c2b145cea93d4a8
Kiidle <jonas.buechi@4teamwork.ch>
2023-03-13 14:51:27 +0100

    Add CreateView for Feed and fix GUI CreateView Commentary

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from .views import FeedsView, FeedView
+from .views import FeedsView, FeedView, FeedCreateView
 
 urlpatterns = [
     path("", login_required(FeedsView.as_view()), name="feeds"),
+    path("feeds/create", FeedCreateView.as_view(), name="feed_create"),
     path("feeds/<slug:pk>", FeedView.as_view(), name="feed")
 ]
--- feeds/views.py
+++ feeds/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 from feeds.models import Feed
 from commentary.models import Commentary
 from authentication.models import User
+from django.urls import reverse_lazy
 
 
 class FeedsView(generic.ListView):
@@ -25,3 +26,22 @@ class FeedsView(generic.ListView):
 class FeedView(generic.DetailView):
     model = Feed
     template_name = "feeds/feed.html"
+
+def feed_create():
+    return
+def feed_edit():
+    return
+def feed_delete():
+    return
+
+class FeedCreateView(generic.CreateView):
+    model = Feed
+    fields = ['text']
+    template_name = "feeds/create.html"
+
+    def get_success_url(self):
+        return reverse_lazy('feeds')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
